8e924b2134beff4771139591640ecc83ae65503e
MatsuoKeita <keita-m@k-useful.jp>
2017-05-25 19:03:00 +0900

    第11章途中。

--- web/models/plan_item.ex
+++ web/models/plan_item.ex
@@ -4,7 +4,9 @@ defmodule NanoPlanner.PlanItem do
   schema "plan_items" do
     field :name, :string
     field :description, :string
-    
+    field :starts_at, Timex.Ecto.DateTime
+    field :ends_at, Timex.Ecto.DateTime
+
     timestamps()
   end
 
@@ -15,5 +17,12 @@ defmodule NanoPlanner.PlanItem do
     struct
     |> cast(params, [])
     |> validate_required([])
+  end
+
+  def convert_datetime(items) do
+    alias Timex.Timezone
+    Enum.map items, fn(item) ->
+      Map.merge(item, %{starts_at: Timezone.convert(item.starts_at, "Asia/Tokyo"), ends_at: Timezone.convert(item.ends_at, "Asia/Tokyo")}) 
+    end
   end
 end
